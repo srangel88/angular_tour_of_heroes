b41b8a9d56867083b5cddad1a0a9f263d982ff63
srangel88 <sandra.rangelg88@gmail.com>
2023-04-24 22:16:33 -0400

    heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,6 +10,11 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent {
  
     heroes = HEROES;
+    selectedHero?: Hero;
+
+    onSelect(hero: Hero): void{
+      this.selectedHero = hero;
+    }
   /* hero: Hero = {
     id: 1,
     name: 'Windstorm',
